924d70e99a5c5b11517d7342a4798553a145dc2b
Carolina Pena <caropena000@gmail.com>
2024-07-25 15:04:57 -0300

    entrega final- lógica completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,103 @@
-//Array de objetos de los peluqueros
+//Tomando los id de los bloques HTML
 
-const peluqueros = [
-  {
-    id: 1,
-    nombre: "Juan",
-    especialidad: "Barbería", 
-    disponibilidad:"Martes",
-    descripcion:"Juan es un experimentado barbero experto en cortes de ayer y de hoy. Apto para hombres de todas las generaciones.",
-    horas:["12:00 hs", "13:30 hs", "15:00 hs"],
-    foto: "./assets/images/juan-barbero.png",
-  },
-  {  id: 2,
-    nombre:"Marta", 
-    especialidad: "Corte", 
-    disponibilidad:"Miércoles",
-    descripcion:"Marta es una apasionada del corte, tiene el poder de saber qué corte necesita cada cliente con solo verlos cruzar la puerta.",
-    horas:["13:00 hs", "14:45 hs", "16:00 hs"],
-    foto: "./assets/images/marta-corte.png",
-  },
-  {
-    id: 3,
-    nombre:"Rocío", 
-    especialidad: "Colorista", 
-    disponibilidad:"Jueves",
-    descripcion:"La nueva integrante de la peluquería, Rocío le encanta experimentar y probar colores nuevos. Si buscás un rotundo cambio de look, es por ahí.",
-    horas:["12:30 hs", "15:30 hs", "17:00 hs"],
-    foto: "./assets/images/rocio-colorista.png",
-  },
-  {
-    id: 4,
-    nombre:"Mario", 
-    especialidad: "Corte", 
-    disponibilidad:"Viernes",
-    descripcion:"El mejor con los niños(y grandes también), te cortás el pelo y, de paso, hacés terapia. Un peluquero tradicional.",
-    horas:["15:30 hs", "16:00 hs", "17:30 hs"],
-    foto: "./assets/images/mario-corte.png",
-  },
-  {
-    id: 5,
-    nombre:"Éric", 
-    especialidad: "Colorista", 
-    disponibilidad:"Sábado",
-    descripcion:"El más experimentado del grupo. Elige los productos de mejor calidad que mejor se adapten a tu cabello.",
-    horas:["9:30 hs", "10:00 hs", "11:00 hs"],
-    foto: "./assets/images/eric-colorista.png",
-  },
-  {
-    id: 6,
-    nombre:"Marcelo", 
-    especialidad: "Barbería", 
-    disponibilidad:"Lunes",
-    descripcion:"Marcelo busca constantemente innovar y reversionar cortes clásicos para hacer destacar su arte. Si buscás un estilo único, es el indicado.",
-    horas:["15:30 hs", "16:30 hs", "17:30 hs"],
-    foto: "./assets/images/marcelo-barberia.png",
-  },
-];
+const peluquerosContainer = document.querySelector('#peluqueros-container');
+const formContainer = document.querySelector('#form-container');
+const mensajeContainer = document.querySelector('#mensaje-container');
+const tablaContainer = document.querySelector('#tabla-container');
+
+//Fecth de peluqueros + creación dinámica de tarjetas y formulario para inscribir turnos
+
+  const fetchPeluqueros = async () => {
+    try {
+      const response = await fetch('./assets/json/peluqueros.json');
+      if (!response.ok) {
+        throw new Error('Error al cargar el archivo JSON');
+      }
+      const peluqueros = await response.json();
+      return peluqueros;
+    } catch (error) {
+      Swal.fire({
+        title: '¡Error!',
+        text: 'No se encuentran disponibles los peluqueros. intente más tarde.',
+        icon: 'error',
+        background: "rgb(98, 128, 230)",
+        color: "whitesmoke",
+        width: 250,
+        confirmButtonText: 'Ok',
+        customClass: {
+          confirmButton: 'custom-button',
+        },
+      });
+    }
+  };
+
+  const cargarPeluqueros = async () => {
+    const peluqueros = await fetchPeluqueros();
+    if (peluqueros) {
+      peluqueros.forEach(peluquero => {
+        const peluqueroCard = document.createElement('div');
+        peluqueroCard.className = 'peluquero-card';
+        peluqueroCard.innerHTML = `
+          <img src="${peluquero.foto}" alt="${peluquero.nombre}">
+          <h3>${peluquero.nombre}</h3>
+          <p>${peluquero.especialidad}</p>
+          <p>Disponible: ${peluquero.disponibilidad}</p>
+          <p>${peluquero.descripcion}</p>
+          <button data-id="${peluquero.id}">Seleccionar</button>
+        `;
+        peluquerosContainer.appendChild(peluqueroCard);
+      });
+  
+      peluquerosContainer.addEventListener('click', (event) => {
+        if (event.target.tagName === 'BUTTON') {
+          const peluqueroId = event.target.getAttribute('data-id');
+          const peluquero = peluqueros.find(p => p.id == peluqueroId);
+          showForm(peluquero);
+          limpiarMensaje();
+          document.getElementById('form-container').scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    }
+  };
+  
+  const limpiarMensaje = () => {
+    mensajeContainer.innerHTML = '';
+  };
+  
+  cargarPeluqueros();
+
+
+  const showForm = (peluquero, turno) => {
+    formContainer.innerHTML = `
+      <h2>${turno ? 'Editar' : 'Confirmar'} Turno con ${peluquero.nombre}</h2>
+      <form id="turno-form">
+        <p>*Campos obligatorios</p>
+        <label for="nombre">Nombre*:</label>
+          <input type="text" id="nombre" name="nombre" value="${turno ? turno.nombre : ''}" required>
+        <label for="apellido">Apellido*:</label>
+          <input type="text" id="apellido" name="apellido" value="${turno ? turno.apellido : ''}" required>
+        <label for="telefono">Teléfono*:</label>
+          <input type="number" id="telefono" name="telefono" value="${turno ? turno.telefono : ''}" required>
+        <label for="horario">Seleccionar Horario:</label>
+          <select id="horario" name="horario">
+            ${peluquero.horas.map(hora => `<option value="${hora}" ${turno && turno.horario === hora ? 'selected' : ''}>${hora}</option>`).join('')}
+          </select>
+        <button type="submit">${turno ? 'Actualizar Turno' : 'Confirmar Turno'}</button>
+      </form>
+    `;
+  
+    document.getElementById('turno-form').addEventListener('submit', (event) => {
+      event.preventDefault();
+      const nombre = document.getElementById('nombre').value;
+      const apellido = document.getElementById('apellido').value;
+      const telefono = document.getElementById('telefono').value;
+      const horario = document.getElementById('horario').value;
+      if (turno) {
+        actualizarTurno(turno.id, peluquero, nombre, apellido, telefono, horario);
+      } else {
+        guardarTurno(peluquero, nombre, apellido, telefono, horario);
+      }
+      mensajeConfirmacion(peluquero, nombre, apellido, telefono, horario);
+      turnosAgendados();
+    });
+  };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,148 +1,121 @@
-//Tomando los id de los bloques HTML
-const peluquerosContainer = document.querySelector('#peluqueros-container');
-const formContainer = document.querySelector('#form-container');
-const mensajeContainer = document.querySelector('#mensaje-container');
-const tablaContainer = document.querySelector('#tabla-container');
+//Funciones y otras funcionalidades alrededor del guardado de turnos
 
-//Creación de tarjetas
-  peluqueros.forEach(peluquero => {
-    const peluqueroCard = document.createElement('div');
-    peluqueroCard.className = 'peluquero-card';
-    peluqueroCard.innerHTML = `
-      <img src="${peluquero.foto}" alt="${peluquero.nombre}">
-      <h3>${peluquero.nombre}</h3>
-      <p>${peluquero.especialidad}</p>
-      <p>Disponible: ${peluquero.disponibilidad}</p>
-      <p>${peluquero.descripcion}</p>
-      <button data-id="${peluquero.id}">Seleccionar</button>
-    `;
-    peluquerosContainer.appendChild(peluqueroCard);
-  });
-//Eventos del botón Seleccionar
-  peluquerosContainer.addEventListener('click', (event) => {
-    if (event.target.tagName === 'BUTTON') {
-      const peluqueroId = event.target.getAttribute('data-id');
-      const peluquero = peluqueros.find(p => p.id == peluqueroId);
-      showForm(peluquero);
-      limpiarMensaje();
-      document.getElementById('form-container').scrollIntoView({ behavior: 'smooth' });
-    }
-  });
+const guardarTurno = (peluquero, nombre, apellido, telefono, horario) => {
+  const turno = {
+    id: Date.now(),
+    peluquero: peluquero.nombre,
+    nombre: nombre,
+    apellido: apellido,
+    telefono: telefono,
+    dia: peluquero.disponibilidad,
+    horario: horario
+  };
+  let turnos = JSON.parse(localStorage.getItem('turnos')) || [];
 
-//Creación del formulario
-  function showForm(peluquero) {
-    formContainer.innerHTML = `
-      <h2>Confirmar Turno con ${peluquero.nombre}</h2>
-      <form id="turno-form">
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required>
-        <label for="apellido">Apellido:</label>
-        <input type="text" id="apellido" name="apellido" required>
-        <label for="telefono">Teléfono:</label>
-        <input type="tel" id="telefono" name="telefono" required>
-        <label for="horario">Seleccionar Horario:</label>
-        <select id="horario" name="horario">
-          ${peluquero.horas.map(hora => `<option value="${hora}">${hora}</option>`).join('')}
-        </select>
-        <button type="submit">Confirmar Turno</button>
-      </form>
-    `;
-
-    document.getElementById('turno-form').addEventListener('submit', (event) => {
-      event.preventDefault();
-      const nombre = document.getElementById('nombre').value;
-      const apellido = document.getElementById('apellido').value;
-      const telefono = document.getElementById('telefono').value;
-      const horario = document.getElementById('horario').value;
-      guardarTurno(peluquero, nombre, apellido, telefono, horario);
-      mensajeConfirmacion(peluquero, nombre, apellido, telefono, horario);
-      turnosAgendados();
-    });
-  }
-//guardado de turno
-  function guardarTurno(peluquero, nombre, apellido, telefono, horario) {
-    const turno = {
-      id: Date.now(),
-      peluquero: peluquero.nombre,
-      nombre: nombre,
-      apellido: apellido,
-      telefono: telefono,
-      dia: peluquero.disponibilidad,
-      horario: horario
-    };
-    let turnos = JSON.parse(localStorage.getItem('turnos')) || [];
+  // Filtrar turnos duplicados
+  const turnoExistente = turnos.some(t => t.nombre === nombre && t.apellido === apellido && t.telefono === telefono && t.peluquero === peluquero.nombre && t.horario === horario);
+  if (!turnoExistente) {
     turnos.push(turno);
     localStorage.setItem('turnos', JSON.stringify(turnos));
+  } else {
+    Swal.fire({
+      title: `Ya tienes un turno agendado`,
+      text: `A nombre de ${nombre} ${apellido} en el horario de ${horario} con ${peluquero.nombre}. Por favor, modificá el turno`,
+      imageUrl: "./assets/images/warning.jpg",
+      imageWidth: 200,
+      imageHeight: 200,
+      width: 300,
+      imageAlt: "alert",
+      background: "rgb(98, 128, 230)",
+      color: "whitesmoke",
+      width: 250,
+      confirmButtonText: 'Ok',
+      customClass: {
+        confirmButton: 'custom-button',
+      }
+    });
   }
-//Mensaje una vez guardado el turno
-  function mensajeConfirmacion(peluquero, nombre, apellido, telefono, horario) {
-    mensajeContainer.innerHTML = `
-      <div class="confirmacion">
-        <p>Muy bien ${nombre} ${apellido}, confirmaste turno con ${peluquero.nombre} el día ${peluquero.disponibilidad} a las ${horario}. En unos días te enviamos un mensaje al ${telefono} para recordarte el turno.</p>
-      </div>
-    `;
+};
+
+const actualizarTurno = (id, peluquero, nombre, apellido, telefono, horario) => {
+  let turnos = JSON.parse(localStorage.getItem('turnos')) || [];
+  turnos = turnos.map(turno => turno.id === id ? { ...turno, peluquero: peluquero.nombre, nombre, apellido, telefono, dia: peluquero.disponibilidad, horario } : turno);
+  localStorage.setItem('turnos', JSON.stringify(turnos));
+};
+
+const mensajeConfirmacion = (peluquero, nombre, apellido, telefono, horario) => {
+  const mensajeContainer = document.querySelector('#mensaje-container');
+  mensajeContainer.innerHTML = `
+    <div class="confirmacion">
+      <p>Muy bien ${nombre} ${apellido}, confirmaste turno con ${peluquero.nombre} el día ${peluquero.disponibilidad} a las ${horario}. En unos días te enviaremos un mensaje al ${telefono} para recordarte el turno.</p>
+    </div>
+  `;
+};
+
+const turnosAgendados = () => {
+  const tablaContainer = document.querySelector('#tabla-container');
+  const turnos = JSON.parse(localStorage.getItem('turnos')) || [];
+  if (turnos.length === 0) {
+    tablaContainer.innerHTML = '<p>No hay turnos agendados.</p>';
+    return;
   }
 
-  function limpiarMensaje() {
-    mensajeContainer.innerHTML = '';
-  }
-
-  //Tabla de turnos registrados
-  function turnosAgendados() {
-    const turnos = JSON.parse(localStorage.getItem('turnos')) || [];
-    if (turnos.length === 0) {
-      tablaContainer.innerHTML = '<p>No hay turnos agendados.</p>';
-      return;
-    }
-
-    const tablaTurnos = `
-      <table>
-        <thead>
-          <tr>
-            <th>Peluquero</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Teléfono</th>
-            <th>Día</th>
-            <th>Horario</th>
-            <th>Acciones</th>
+  const tablaTurnos = `
+    <table>
+      <thead>
+        <tr>
+          <th>Peluquero</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Teléfono</th>
+          <th>Día</th>
+          <th>Horario</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${turnos.map(turno => `
+          <tr data-id="${turno.id}">
+            <td>${turno.peluquero}</td>
+            <td>${turno.nombre}</td>
+            <td>${turno.apellido}</td>
+            <td>${turno.telefono}</td>
+            <td>${turno.dia}</td>
+            <td>${turno.horario}</td>
+            <td>
+              <button class="editar-btn" data-id="${turno.id}">Editar</button>
+              <button class="cancelar-btn" data-id="${turno.id}">Cancelar</button>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          ${turnos.map(turno => `
-            <tr data-id="${turno.id}">
-              <td>${turno.peluquero}</td>
-              <td>${turno.nombre}</td>
-              <td>${turno.apellido}</td>
-              <td>${turno.telefono}</td>
-              <td>${turno.dia}</td>
-              <td>${turno.horario}</td>
-              <td><button class="cancelar-btn" data-id="${turno.id}">Cancelar</button></td>
-            </tr>
-          `).join('')}
-        </tbody>
-      </table>
-    `;
-    tablaContainer.innerHTML = tablaTurnos;
-  }
-
-  //elimina el registro de turnos del localStorage
-  tablaContainer.addEventListener('click', (event) => {
-    if (event.target.classList.contains('cancelar-btn')) {
-      const turnoId = event.target.getAttribute('data-id');
-      cancelarTurno(turnoId);
-    }
-  });
+        `).join('')}
+      </tbody>
+    </table>
+  `;
+  tablaContainer.innerHTML = tablaTurnos;
+};
 
 
-  function cancelarTurno(turnoId) {
-    let turnos = JSON.parse(localStorage.getItem('turnos')) || [];
-    turnos = turnos.filter(turno => turno.id != turnoId);
-    localStorage.setItem('turnos', JSON.stringify(turnos));
-    turnosAgendados();
-    limpiarMensaje();
-  }
-
-  // Mostrar turnos agendados al cargar la página
+const cancelarTurno = (turnoId) => {
+  let turnos = JSON.parse(localStorage.getItem('turnos')) || [];
+  turnos = turnos.filter(turno => turno.id != turnoId);
+  localStorage.setItem('turnos', JSON.stringify(turnos));
   turnosAgendados();
-;
+  limpiarMensaje();
+};
+
+document.querySelector('#tabla-container').addEventListener('click', (event) => {
+  const turnoId = event.target.getAttribute('data-id');
+  if (event.target.classList.contains('cancelar-btn')) {
+    cancelarTurno(turnoId);
+  } else if (event.target.classList.contains('editar-btn')) {
+    let turnos = JSON.parse(localStorage.getItem('turnos')) || [];
+    const turno = turnos.find(t => t.id == turnoId);
+    fetchPeluqueros().then(peluqueros => {
+      const peluquero = peluqueros.find(p => p.nombre == turno.peluquero);
+      showForm(peluquero, turno);
+      limpiarMensaje();
+    });
+  }
+});
+
+turnosAgendados();
